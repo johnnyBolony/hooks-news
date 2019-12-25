2ba0caff98bf1b9e1531abb4aa2a7c0aa90152ae
johnnybolony <ctexyg@gmail.com>
2019-12-25 13:11:48 +0200

    Updates

---
+++ precache-manifest.5c3555ac0f233db2723196303af3bf35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cd4119a764689deaa9e631e2266078",
+    "url": "/hooks-news/index.html"
+  },
+  {
+    "revision": "405615406972e2a8c4e2",
+    "url": "/hooks-news/static/css/main.44a987e0.chunk.css"
+  },
+  {
+    "revision": "cf3d33172cffd6830ab4",
+    "url": "/hooks-news/static/js/2.d66ebe27.chunk.js"
+  },
+  {
+    "revision": "cab85fafb6d7ef48523940bce44f0a07",
+    "url": "/hooks-news/static/js/2.d66ebe27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "405615406972e2a8c4e2",
+    "url": "/hooks-news/static/js/main.96c7e07e.chunk.js"
+  },
+  {
+    "revision": "303480cab3e2fefc4e76",
+    "url": "/hooks-news/static/js/runtime-main.22d4f003.js"
+  }
+]);
